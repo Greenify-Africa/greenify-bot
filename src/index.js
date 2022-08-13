71c6887b80dc18454b7adfd4e64eb5af6516cf4e
John Daniels <adeyemijohndaniels@gmial.com>
2022-08-13 11:12:08 +0100

    added express to our bot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+const express = require("express")
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.get("/*", (req, res) => {
+  res.send("Welcome to Greenify bot 🚀")
+})
+
+app.listen(port, () =>
+  console.log(`Greenify bot (Twit Hunter 🤖) is up on port ${port}🚀`)
+)
+
 require("./bot")
